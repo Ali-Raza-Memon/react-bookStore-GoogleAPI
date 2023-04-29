71ff59f90833eccad6bfa7722b2fa179be6bcf49
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-04-29 19:32:38 +0500

    Getting data from Google API

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Card = (book) => {
+
+
+    <>
+
+        {
+            book.map((item) => {
+
+                return (
+                    <>
+                    <div className="card">
+                        <img src="./images/book.jpg" alt="" />
+                        <div className="bottom">
+                            <h3 className="title"> React Js</h3>
+                            <p className="amount">&#8377;3290</p>
+                        </div>
+
+                    </div>
+                    </>
+                )
+
+            })
+
+        }  
+
+    </>
+       
+
+}
+export default Card
